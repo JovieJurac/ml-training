f8d1c612fa142267029aab61bfb715d275b628c6
JovieJurac <jovie.jurac@mlhuillier.com>
2024-05-29 17:01:49 +0800

    [JS-5] Created a function to convert Celcius and Fahrenheit vice versa

--- week-1/javascript/5.js
+++ week-1/javascript/5.js
@@ -4,3 +4,16 @@
  * @param {number} celsius The temperature in Celsius.
  * @return {number} The temperature in Fahrenheit.
  */
+
+function celcius(temperature){
+    const fahrenheit = 0.4285714285714286;
+    return parseInt(Math.fround( temperature * fahrenheit)) ;
+}
+
+function fahrenheit(temperature){
+    const celcius = 2.33333333333;
+    return  parseInt(Math.fround(temperature * celcius));
+}
+
+console.log( "Fahrenheit to Celcius = " + celcius(140) + "°C" );
+console.log( "Celcius to Fahrenheit = " + fahrenheit(60) + "°F");
