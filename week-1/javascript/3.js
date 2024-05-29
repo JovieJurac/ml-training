9e054f8b76c3f047c2def278dcf91b0658a5a47d
JovieJurac <jovie.jurac@mlhuillier.com>
2024-05-29 16:39:19 +0800

    [JS-3] Created a function to display Fizz, Buzz, FizzBuzz on a defined condition

--- week-1/javascript/3.js
+++ week-1/javascript/3.js
@@ -5,3 +5,21 @@
  * For numbers which are multiples of both three and five print 'FizzBuzz'.
  * @param {number} n The number to print.
  */
+
+function fizzBuzz(num){
+    // let checker = 1;
+
+    for ( let checker = 1; checker <= num; checker++  ){
+        if( checker % 3 == 0 && checker % 5 == 0 ){
+            console.log("FizzBuzz");
+        }else if ( checker % 3 == 0 ) {
+            console.log( "Fizz" )
+        }else if ( checker % 5 == 0 ) {
+            console.log("Buzz")
+        }else{
+            console.log(checker);
+        }
+    }
+}
+
+const num = fizzBuzz(30);
