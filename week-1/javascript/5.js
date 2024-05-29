8224cd2f2fb61d96f4a42625886239dd6f8aea7e
JovieJurac <jovie.jurac@mlhuillier.com>
2024-05-30 07:14:08 +0800

    [JS-5] Updating the function since it was interchanged

--- week-1/javascript/5.js
+++ week-1/javascript/5.js
@@ -5,8 +5,9 @@
  * @return {number} The temperature in Fahrenheit.
  */
 
-function celcius(temperature){
-    const fahrenheit = 0.4285714285714286;
-    return console.log( "Fahrenheit to Celcius = " + parseInt(Math.fround( temperature * fahrenheit))+ "°C" );
+function fahrenheit(temperature){
+    const celcius = 2.33333333333;
+    return  parseInt(Math.fround(temperature * celcius));
 }
-celcius(140);
+
+console.log( "Celcius to Fahrenheit = " + fahrenheit(60) + "°F");
