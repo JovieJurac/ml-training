df88a7dc14e3ca81ae3ab5f2c6f3e8264f31fa6e
JovieJurac <jovie.jurac@mlhuillier.com>
2024-05-29 16:24:59 +0800

    [JS-2] Created a function to find the sum of set of numbers

--- week-1/javascript/2.js
+++ week-1/javascript/2.js
@@ -3,3 +3,16 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+
+function sumArray(...numbers){
+    console.log(numbers)
+    let total = 0;
+
+    for (const num of numbers){
+        console.log(num);
+        total += num;
+    }
+    return console.log(total);
+}
+
+const number = sumArray(1,2,3);
