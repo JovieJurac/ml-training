203a2d695398c4ee60476885403471cea43e60aa
JovieJurac <jovie.jurac@mlhuillier.com>
2024-05-29 16:06:12 +0800

    [JS-1] Created a function to check an odd/even number

--- week-1/javascript/1.js
+++ week-1/javascript/1.js
@@ -3,3 +3,16 @@
  * @param {number} num The number to check.
  * @return {boolean} True if the number is even, false if it is odd.
  */
+
+
+
+function numCheck(num) {
+    if( num % 2 == 0 ){
+        return console.log( `The number ${num} is even.` );
+    } else {
+        return console.log( `The number ${num} is odd.`);
+    }
+}
+
+const oddNum = numCheck(3);
+const evenNum = numCheck(10);
