368cedb5992067807654a49d20947ef9e2bd3d80
JovieJurac <jovie.jurac@mlhuillier.com>
2024-05-30 10:21:45 +0800

    [JS-15] Created a function to get the lowest and highest value of an array

--- week-1/javascript/14.js
+++ week-1/javascript/14.js
@@ -6,3 +6,9 @@
  * Example array: [5, 2, 8, 1, 9, 3]
  * Expected output: "The lowest number is 1 and the highest number is 9."
  */
+
+function finder(...arr){
+    const newArr = arr.sort();
+    console.log(`The lowest value in the array is ${newArr[0]} while the highest is ${newArr[arr.length - 1]}.`);
+}
+finder(5,4,2,8,1,7);
