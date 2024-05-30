67d3d719b27b62dde49caaa43321929785dc3851
JovieJurac <jovie.jurac@mlhuillier.com>
2024-05-30 09:54:33 +0800

    [JS-12] Created a function to count the length of an array

--- week-1/javascript/12.js
+++ week-1/javascript/12.js
@@ -3,3 +3,14 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+
+function emptyCheck(...arr){
+    if(arr.length == 0){
+        console.log("The array is empty");
+    } else {
+        console.log(arr);
+    }
+}
+
+emptyCheck(1,2,3,4,5);
