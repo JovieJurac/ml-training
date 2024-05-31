82d7cb8888d3142e4c60c20e34bb6953ad542a33
JovieJurac <jovie.jurac@mlhuillier.com>
2024-05-31 13:46:49 +0800

    [JS-4] Created a function that displays a current date to a specific format

--- week-1/javascript/4.js
+++ week-1/javascript/4.js
@@ -3,3 +3,20 @@
  * 'MM-DD-YYYY HH:MM:SS'.
  * @return {string} The current date and time in the format 'MM-DD-YYYY HH:MM:SS'.
  */
+
+
+
+function formatDate(date){
+    const format = (num) => num.toString().padStart(2,0);
+
+    const month = format(date.getMonth()+1);
+    const day = format(date.getDate());
+    const year = date.getFullYear();
+    const hour = format(date.getHours());
+    const min = format(date.getMinutes());
+    const sec = format(date.getSeconds());
+    
+    return console.log(`${month}-${day}-${year} ${hour}:${min}:${sec}`);
+}
+
+formatDate(new Date);
