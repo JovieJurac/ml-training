61404c5e46126e52b798879807aaceaf5fe51fda
JovieJurac <jovie.jurac@mlhuillier.com>
2024-06-01 14:27:18 +0800

    [TS-5] Created a function to translate celcius to fahrenheit

--- week-1/typescript/5.ts
+++ week-1/typescript/5.ts
@@ -4,3 +4,10 @@
  * @param {number} celsius The temperature in Celsius.
  * @return {number} The temperature in Fahrenheit.
  */
+
+function fahrenheit(temperature: number){
+    const celcius: number = 2.33333333333;
+    return  parseInt(Math.fround(temperature * celcius).toString());
+}
+
+console.log( "Celcius to Fahrenheit = " + fahrenheit(60) + " °F");
