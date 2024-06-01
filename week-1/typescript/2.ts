d2570937baf368b3daee1f07e5ad2233b04037c4
JovieJurac <jovie.jurac@mlhuillier.com>
2024-06-01 14:18:30 +0800

    [TS-2] Created a function to get the sum of an array of numbers

--- week-1/typescript/2.ts
+++ week-1/typescript/2.ts
@@ -3,3 +3,12 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+
+function sumArray(...numbers: number[]): number {
+  let total: number = 0;
+  for (const num of numbers) {
+    total += num;
+  }
+  return total;
+}
+console.log(sumArray(1, 2, 3));
