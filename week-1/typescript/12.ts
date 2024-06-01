5c087d09608ca488d58e81ce0c7fe3ad3f1ff11d
JovieJurac <jovie.jurac@mlhuillier.com>
2024-06-01 14:40:47 +0800

    [TS-12] Created a function to check if an array is empty

--- week-1/typescript/12.ts
+++ week-1/typescript/12.ts
@@ -3,3 +3,13 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+function emptyCheck(...arr: number[]):void{
+    if(arr.length == 0){
+        console.log("The array is empty!");
+    } else {
+        console.log(arr);
+    }
+}
+
+emptyCheck(89,98,9);
