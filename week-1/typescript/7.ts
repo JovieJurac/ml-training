e1779a0817a2ae57f0634444043b19d4e94d7d9d
JovieJurac <jovie.jurac@mlhuillier.com>
2024-06-01 14:30:49 +0800

    [TS-7] Created a function to check if the number is an integer

--- week-1/typescript/7.ts
+++ week-1/typescript/7.ts
@@ -3,3 +3,17 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+function checkInteger(num:number) {
+    try {
+      if (!Number.isInteger(num)) {
+        throw "The inputted value ain't an integer!";
+      } else {
+        console.log(`The inputted value is ${num}`);
+      }
+    } catch (error) {
+      return console.log(error);
+    }
+  }
+  
+  checkInteger(-2);
