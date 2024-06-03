bc88da4a12093650f87205d10872de5c34422c83
JovieJurac <jovie.jurac@mlhuillier.com>
2024-06-03 13:17:17 +0800

    [TS-18] Created a function that applies a simple CRUD function

--- week-1/typescript/18.ts
+++ week-1/typescript/18.ts
@@ -16,3 +16,87 @@
  * @param {string[]} items
  * @returns {string}
  */
+
+interface item {
+  name: string;
+  price: number;
+}
+
+class ProductManager {
+  private cart: item[];
+
+  constructor(products: item[] = []) {
+    this.cart = products;
+  }
+
+  addProducts(product: item): void {
+    try {
+      if (this.cart.length > 9) {
+        throw " Maximum of 10 items only.";
+      } else {
+        this.cart.push(product);
+      }
+    } catch (error) {
+      return console.log(error);
+    }
+  }
+
+  deleteProducts(product: item): void {
+    for (let i  = 0; i < this.cart.length; i++) {
+      if (this.cart[i].name == product.name) {
+        this.cart.splice(i, 1);
+        return;
+      }
+    }
+  }
+
+  displayTotal(): void {
+    let total = 0;
+    this.cart.forEach((product) => {
+      total += product.price;
+    });
+    return console.log(`The total amount on your cart is ${total} in pesos.`);
+  }
+
+  displayQty(): void {
+    return console.log(
+      `With the total quantity of, ${this.cart.length} items.`
+    );
+  }
+
+  displayCart(): item[] {
+    return this.cart;
+  }
+}
+
+const cart = new ProductManager([
+  { name: "Banana", price: 8.0 },
+  { name: "Apple", price: 10.0 },
+  { name: "Mango", price: 30.0 },
+]);
+
+console.log(cart.displayCart());
+cart.addProducts({ name: "Orange", price: 15.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Pomelon", price: 74.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Grapes", price: 54.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Guava", price: 13.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Cherry", price: 46.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Coconut", price: 23.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Tomato", price: 4.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Strawberry", price: 55.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Watermelon", price: 65.0 });
+console.log(cart.displayCart());
+cart.addProducts({ name: "Kiwi", price: 25.0 });
+console.log(cart.displayCart());
+cart.deleteProducts({name: "Mango", price: 30.0});
+console.log(cart.displayCart());
+cart.displayTotal();
+cart.displayQty();
